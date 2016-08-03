a0a49edc9df1003e61ee44724311dbf1c68390b7
sblatz <sblatz@purdue.edu>
2016-08-03 16:13:46 -0400

    Initial Commit

---
+++ Hall PassTests/Hall_PassTests.swift
@@ -0,0 +1,36 @@
+//
+//  Hall_PassTests.swift
+//  Hall PassTests
+//
+//  Created by Sawyer Blatz on 8/3/16.
+//  Copyright © 2016 Sawyer Blatz. All rights reserved.
+//
+
+import XCTest
+@testable import Hall_Pass
+
+class Hall_PassTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
